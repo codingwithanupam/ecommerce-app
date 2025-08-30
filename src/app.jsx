cf90d96b6ef1d2bb33bc1db4a4632193e1d2fd08
codingwithanupam <aicodingwithanupam@gmail.com>
2025-08-30 16:42:44 +0530

    it is my first commit

---
+++ src/app.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+  let isLogeIn = false;
+  // let content;
+  // if (isLogeIn) content = <Admin />;
+  // else content = <User />;
+
+  // console.log(content, typeof content);
+  return (
+    <>
+      <MyButtons />
+      <MyButtons />
+    </>
+  );
+}
+
+function MyButtons() {
+  let [count, setCount] = useState(0);
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      click {count} times
+    </button>
+  );
+}
+
+export default App;
---
+++ src/c1.jsx
@@ -0,0 +1,6 @@
+import C2 from "./c2.jsx";
+function C1() {
+  return <C2 />;
+}
+
+export default C1;
